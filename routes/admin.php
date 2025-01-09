c6019abff12cf7df845e73195b6d0fb12b1529ed
Insath97 <insath1997.mi@gmail.com>
2025-01-10 02:07:53 +0530

    Master Blade setup done

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\Admin\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
 
     /* login */
+    Route::get('login', [AuthController::class, 'login'])->name('login');
 });
